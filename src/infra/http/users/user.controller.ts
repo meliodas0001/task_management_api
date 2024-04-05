1742715266a9cbc91aa662abe4191fe11468e9b0
meliodas0001 <riquemorozinee@gmail.com>
2024-04-05 18:03:28 -0300

    feat: jest config

--- src/infra/http/users/user.controller.ts
+++ src/infra/http/users/user.controller.ts
@@ -1,10 +1,9 @@
 import { IUserCreate } from '@domains/requests/users/users';
-import { UserService } from '@domains/services/user/user.service';
-import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
-import { Response } from 'express';
-import { ValidatorPipe } from 'src/app/utils/validators/pipes/validatorPipes';
-import { CreateUserSchema } from 'src/app/utils/validators/schemas/User/createUser';
-import { PrismaService } from 'src/infra/database/prisma.service';
+import { UserService } from '@app/services/user/user.service';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ValidatorPipe } from '@app/utils/validators/pipes/validatorPipes';
+import { CreateUserSchema } from '@app/utils/validators/schemas/User/createUser';
+import { PrismaService } from '../../database/prisma.service';
 
 @Controller('users')
 export class UserController {
@@ -21,12 +20,11 @@ export class UserController {
   @Post('/create')
   async createUser(
     @Body(new ValidatorPipe(CreateUserSchema)) body: IUserCreate,
-    @Res() res: Response,
-  ): Promise<void> {
+  ) {
     await this.prismaService.$transaction(async (transaction) => {
       return await this.userService.createUser(body, transaction);
     });
 
-    res.json().status(201);
+    return 201;
   }
 }
--- src/infra/http/users/user.module.ts
+++ src/infra/http/users/user.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
-import { UserService } from '@domains/services/user/user.service';
+import { UserService } from 'src/app/services/user/user.service';
 import { PrismaService } from 'src/infra/database/prisma.service';
 import { DatabaseModule } from 'src/infra/database/database.module';
 
