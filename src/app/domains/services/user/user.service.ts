fdd4365d7e32cbbb2bbaec8b5af866dc48b04fb5
meliodas0001 <riquemorozinee@gmail.com>
2024-04-05 17:18:12 -0300

    feat: password hash with bcrypt

--- src/app/domains/services/user/user.service.ts
+++ src/app/domains/services/user/user.service.ts
@@ -1,13 +1,17 @@
+import * as bcrypt from 'bcrypt';
 import { ORMTransactionInstance } from '@domains/database/ORM';
-import { UserEntity } from '@domains/database/entities/User/UserEntity';
 import { IUserRepository } from '@domains/database/repositories/UserRepository/IUserRepository';
+import { IUserCreate } from '@domains/requests/users/users';
 import { ConflictException, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: IUserRepository) {}
 
-  async createUser(user, transaction: ORMTransactionInstance): Promise<void> {
+  async createUser(
+    user: IUserCreate,
+    transaction: ORMTransactionInstance,
+  ): Promise<void> {
     const findUser = await this.userRepository.findByEmail(
       user.email,
       transaction,
@@ -16,6 +20,11 @@ export class UserService {
     if (findUser) {
       throw new ConflictException('User already exists');
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(user.password, salt);
+
+    user.password = hashedPassword;
 
     await this.userRepository.createUser(user, transaction);
   }
--- src/infra/http/users/user.controller.ts
+++ src/infra/http/users/user.controller.ts
@@ -1,7 +1,9 @@
-import { UserEntity } from '@domains/database/entities/User/UserEntity';
+import { IUserCreate } from '@domains/requests/users/users';
 import { UserService } from '@domains/services/user/user.service';
 import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
+import { ValidatorPipe } from 'src/app/utils/validators/pipes/validatorPipes';
+import { CreateUserSchema } from 'src/app/utils/validators/schemas/User/createUser';
 import { PrismaService } from 'src/infra/database/prisma.service';
 
 @Controller('users')
@@ -17,11 +19,14 @@ export class UserController {
   }
 
   @Post('/create')
-  async createUser(@Body() user, @Res() res: Response): Promise<void> {
+  async createUser(
+    @Body(new ValidatorPipe(CreateUserSchema)) body: IUserCreate,
+    @Res() res: Response,
+  ): Promise<void> {
     await this.prismaService.$transaction(async (transaction) => {
-      return await this.userService.createUser(user, transaction);
+      return await this.userService.createUser(body, transaction);
     });
 
-    res.status(201);
+    res.json().status(201);
   }
 }
