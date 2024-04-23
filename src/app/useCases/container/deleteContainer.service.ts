64d7fb71596e17bb4ad981dd7f8ee158a27c1508
meliodas0001 <riquemorozinee@gmail.com>
2024-04-23 19:41:36 -0300

    feat: delete container use case

---
+++ src/app/useCases/container/deleteContainer.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+
+import { IContainersRepository } from '@domains/database/repositories/ContainersRepository/IContainersRepository';
+import { ORMTransactionInstance } from '@domains/database/ORM';
+
+@Injectable()
+export class DeleteContainer {
+  constructor(private readonly containersRepository: IContainersRepository) {}
+
+  async execute(
+    containerId: string,
+    userId: string,
+    transaction: ORMTransactionInstance,
+  ) {
+    const container = await this.containersRepository.findById(
+      containerId,
+      transaction,
+    );
+
+    if (!container) {
+      throw new UnauthorizedException('Container not found');
+    }
+
+    if (container.ownerId !== userId) {
+      throw new UnauthorizedException('User not owner of container');
+    }
+
+    return await this.containersRepository.deleteContainer(
+      containerId,
+      transaction,
+    );
+  }
+}
