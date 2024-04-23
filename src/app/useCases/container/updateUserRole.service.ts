227ef246be71e15969a43f446da0eef3761a7dc0
meliodas0001 <riquemorozinee@gmail.com>
2024-04-23 19:39:26 -0300

    feat: updateUserRole use case

---
+++ src/app/useCases/container/updateUserRole.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { IContainersRepository } from '@domains/database/repositories/ContainersRepository/IContainersRepository';
+import { ORMTransactionInstance } from '@domains/database/ORM';
+import { Roles } from '@prisma/client';
+
+export class UpdateUserRoles {
+  constructor(private readonly containersRepository: IContainersRepository) {}
+
+  async execute(
+    containerId: string,
+    userId: string,
+    userRole: string,
+    transaction: ORMTransactionInstance,
+  ): Promise<void> {
+    const container = await this.containersRepository.findById(
+      containerId,
+      transaction,
+    );
+
+    if (!container) throw new UnauthorizedException('Container not found');
+
+    let userFind = false;
+
+    container.users.forEach((user) => {
+      if (user.id === userId) {
+        userFind = true;
+      }
+    });
+
+    if (!userFind)
+      throw new UnauthorizedException('User not found in container');
+
+    return await this.containersRepository.updateUserRole(
+      containerId,
+      userId,
+      Roles[userRole],
+      transaction,
+    );
+  }
+}
