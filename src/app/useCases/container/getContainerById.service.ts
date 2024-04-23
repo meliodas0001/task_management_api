a18b22809bb12bb18f91db20a401235cb7505388
meliodas0001 <riquemorozinee@gmail.com>
2024-04-23 19:43:55 -0300

    feat: getContainerById useCase

---
+++ src/app/useCases/container/getContainerById.service.ts
@@ -0,0 +1,35 @@
+import { ORMTransactionInstance } from '@domains/database/ORM';
+import { IContainersRepository } from '@domains/database/repositories/ContainersRepository/IContainersRepository';
+import { UnauthorizedException } from '@nestjs/common';
+
+export class GetContainerById {
+  constructor(private readonly containerRepository: IContainersRepository) {}
+
+  async execute(
+    containerId: string,
+    userId: string,
+    transaction: ORMTransactionInstance,
+  ) {
+    const container = await this.containerRepository.findById(
+      containerId,
+      transaction,
+    );
+
+    if (!container) throw new UnauthorizedException('Container not found');
+
+    if (!container.public) {
+      let userFind = false;
+
+      container.users.forEach((user) => {
+        if (user.id === userId) {
+        }
+        userFind = true;
+      });
+
+      if (!userFind)
+        throw new UnauthorizedException('User not found in container');
+    }
+
+    return container;
+  }
+}
