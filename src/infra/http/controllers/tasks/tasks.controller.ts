1b887af6f9cae0ca811c6af1263b9128a5cedddc
meliodas0001 <riquemorozinee@gmail.com>
2024-04-24 18:42:46 -0300

    feat: taskDelete route

--- src/infra/http/controllers/tasks/tasks.controller.ts
+++ src/infra/http/controllers/tasks/tasks.controller.ts
@@ -1,5 +1,13 @@
 import { Request, Response } from 'express';
-import { Controller, Post, UseGuards, Body, Req, Res } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Body,
+  Req,
+  Res,
+  Delete,
+} from '@nestjs/common';
 
 import { TaskCreateService } from '@app/useCases/tasks/taskCreate.service';
 import { PrismaService } from '@infra/database/prisma.service';
@@ -9,12 +17,14 @@ import { ValidatorPipe } from '@app/utils/validators/pipes/validatorPipes';
 
 import { CreateTaskSchema } from '@app/utils/validators/schemas/Tasks/createTask';
 import { ICreateTaskDTO } from '@domains/requests/tasks/tasksCreate';
+import { TaskDeleteService } from '@app/useCases/tasks/taskDelete.service';
 
 @Controller('containers/folders/tasks')
 export class TasksController {
   constructor(
     private readonly prismaService: PrismaService,
     private readonly taskCreateService: TaskCreateService,
+    private readonly taskDeleteService: TaskDeleteService,
   ) {}
 
   @Post('create')
@@ -38,6 +48,20 @@ export class TasksController {
       await this.taskCreateService.execute(task, transaction);
 
       res.status(201).send();
+    });
+  }
+
+  @Delete('delete')
+  @UseGuards(AuthGuard)
+  async deleteTask(
+    @Body() body: any,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const { taskId } = body;
+
+    await this.prismaService.$transaction(async (transaction) => {
+      await this.taskDeleteService.execute(taskId, transaction);
     });
   }
 }
--- src/infra/http/controllers/tasks/tasks.module.ts
+++ src/infra/http/controllers/tasks/tasks.module.ts
@@ -3,10 +3,11 @@ import { PrismaService } from '@infra/database/prisma.service';
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TaskCreateService } from '@app/useCases/tasks/taskCreate.service';
+import { TaskDeleteService } from '@app/useCases/tasks/taskDelete.service';
 
 @Module({
   controllers: [TasksController],
-  providers: [PrismaService, TaskCreateService],
+  providers: [PrismaService, TaskCreateService, TaskDeleteService],
   imports: [DatabaseModule],
 })
 export class TasksModule {}
