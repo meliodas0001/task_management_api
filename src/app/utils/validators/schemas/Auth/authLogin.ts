c207fa39083820416e43224373cc170157ff092b
meliodas0001 <riquemorozinee@gmail.com>
2024-07-03 20:00:03 -0300

    fix: ILogin interface imported

--- src/app/utils/validators/schemas/Auth/authLogin.ts
+++ src/app/utils/validators/schemas/Auth/authLogin.ts
@@ -1,5 +1,7 @@
 import * as Joi from 'joi';
 
+import { ILogin } from '@domains/requests/auth/login';
+
 export const AuthLoginSchema = Joi.object<ILogin>({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
